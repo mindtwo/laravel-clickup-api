7e3c6af60a01b248cd7be718104f8dea02c5f25f
Levin Hennig <62558218+Hysterikon@users.noreply.github.com>
2025-11-19 10:09:21 +0100

    feat(api-capabilities): update AuthorizedUser endpoint and add ClickUpClient endpoint methods

--- src/Facades/ClickUpClient.php
+++ src/Facades/ClickUpClient.php
@@ -6,19 +6,30 @@ namespace Mindtwo\LaravelClickUpApi\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Mindtwo\LaravelClickUpApi\ClickUpClient as BaseClickUpClient;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\Attachment;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\AuthorizedUser;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\CustomField;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\Folder;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\Milestone;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\Space;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\Subtask;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\Task;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\TaskDependency;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\TaskLink;
+use Mindtwo\LaravelClickUpApi\Http\Endpoints\TaskList;
 
 /**
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\Task tasks()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\Space spaces()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\Folder folders()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\TaskList lists()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\CustomField customFields()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\Attachment attachments()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\AuthorizedUser authorizedUser()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\Subtask subtasks()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\Milestone milestones()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\TaskDependency dependencies()
- * @method static \Mindtwo\LaravelClickUpApi\Http\Endpoints\TaskLink links()
+ * @method static Task tasks()
+ * @method static Space spaces()
+ * @method static Folder folders()
+ * @method static TaskList lists()
+ * @method static CustomField customFields()
+ * @method static Attachment attachments()
+ * @method static AuthorizedUser authorizedUser()
+ * @method static Subtask subtasks()
+ * @method static Milestone milestones()
+ * @method static TaskDependency dependencies()
+ * @method static TaskLink links()
  *
  * @see BaseClickUpClient
  */
